a57a9c5adb8a1795a195ff9eb1311082db39c656
jgb22 <eutuxisfet@gmail.com>
2023-05-27 23:03:44 +0200

    Solved lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    let i = 0
+    this.currentTime = setInterval(()=> {
+      i++
+    },1000)
   }
+  //I couldn't invoke and increment properly for the "Start" method, I have tried lots of scenarios but all fail.
 
   getMinutes() {
-    // ... your code goes here
+    const minutes = Math.floor(this.currentTime / 60)
+    return minutes
+ 
   }
 
-  getSeconds() {
-    // ... your code goes here
-  }
+  getSeconds() { 
+    const seconds = this.currentTime % 60
+    return seconds 
+    }
 
+
+   
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value === 0) {
+      return '00';
+    } else if (value < 10) {
+      return '0' + value.toString();
+    } else {
+      return value.toString();
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval (this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+      this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
   }
+
 }
